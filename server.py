cf328c6a22bddde89eb69834ef01e7d2b11b6675
Holgbob <albinholger04@gmail.com>
2026-05-06 09:02:59 +0200

    Set up session
    I imported session for flask to create a login system that with help of a middleware checked if you where logged in or not and respons sent you either to login or forward depening on that. Also fixed some small faults with password field in html and url_for name

--- server.py
+++ server.py
@@ -1,30 +1,59 @@
 import database
-from flask import Flask, render_template, url_for, request, redirect
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, url_for, request, redirect, session
 
-
-# print(database.badRecipe())
-# print(database.calculateNutrients())
-# print(database.unavailableRecipes())
-
-
+load_dotenv()
 app = Flask(__name__)
 
-#url_for("static", filename = "style.css")
+app.secret_key = f'{os.getenv("SESSION_SECRET_KEY")}'.encode()
 
+### Middlewares ###
+
+# Log 
+@app.before_request
+def log_request():
+    print("\033[34m", f"Method: {request.method}\nURL: {request.url}", "\033[0m")
+    print(request.endpoint)
+
+# Session
+@app.before_request
+def check_session():
+    if request.endpoint not in ["static", "login", "register", "home_page"]: # List of places where you don't need to be logged in
+        if 'email' not in session:
+            return redirect(url_for("login")) # Send to login
+
+### Routes ###
+
+# Base route
 @app.route("/")
-def hello_world():
+def home_page():
     return "<p>Hello, World!</p>"
 
+# Test route
 @app.route("/test")
-def hello_Albin():
+def test():
     return render_template("home.html", person="Albin")
 
+# Login route
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return redirect(url_for("test"))
+        # TO DO: Check if they exist in database
+        session['email'] = request.form['email'] # Add user to session
+        return redirect(url_for("test")) # Send to defualt page for login
 
     return render_template("login.html")
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # TO DO: Check if they exist in database
+        session['email'] = request.form['email'] # Add user to session
+        return redirect(url_for("test")) # Send to defualt page for login
+    return render_template("register.html")
 
-
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('home_page')) # Send back to home page
